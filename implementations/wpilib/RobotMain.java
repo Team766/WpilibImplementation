ff3d307caec2a9e8a43280cf05459c62ca847968
Margaret Chan <mchan20822@gmail.com>
2018-01-20 16:32:57 -0800

    Adds name of config file on robot and catches exceptions in RobotMain

--- implementations/wpilib/RobotMain.java
+++ implementations/wpilib/RobotMain.java
@@ -1,6 +1,7 @@
 package implementations.wpilib;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import interfaces.ConfigFileReader;
 import interfaces.MyRobot;
 import interfaces.RobotProvider;
 
@@ -8,52 +9,103 @@ public class RobotMain extends IterativeRobot implements MyRobot {
 	private MyRobot robot;
 
 	public RobotMain() {
-		RobotProvider.instance = new WPIRobotProvider();
-		this.robot = new com.team766.robot.Robot();
+		try{
+			ConfigFileReader.fileName = "/home/lvuser/robotConfig.txt";
+			RobotProvider.instance = new WPIRobotProvider();
+			this.robot = new com.team766.robot.Robot();
+		}catch(Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void robotInit() {
-		robot.robotInit();
+		try{
+			robot.robotInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void disabledInit() {
-		robot.disabledInit();
+		try{
+			robot.disabledInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void autonomousInit() {
-		robot.autonomousInit();
+		try{
+			robot.autonomousInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void teleopInit() {
-		robot.teleopInit();
+		try{
+			robot.teleopInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void testInit() {
-		robot.teleopInit();
+		try{
+			robot.teleopInit();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void disabledPeriodic() {
-		robot.disabledPeriodic();
+		try{
+			robot.disabledPeriodic();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void autonomousPeriodic() {
-		robot.autonomousPeriodic();
+		try{
+			robot.autonomousPeriodic();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		robot.teleopPeriodic();
+		try{
+			robot.teleopPeriodic();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	@Override
 	public void testPeriodic() {
-		robot.teleopPeriodic();
+		try{
+			robot.teleopPeriodic();
+		}catch (Exception e){
+			e.printStackTrace();
+			throw e;
+		}
 	}
 }
