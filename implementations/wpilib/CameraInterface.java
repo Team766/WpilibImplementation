5d7f5f05c2dcf19e3233f03f13280d46be61625c
Margaret Chan <mchan20822@gmail.com>
2018-03-19 21:11:00 -0700

    Updates camera interface to support only one camera for 2018

--- implementations/wpilib/CameraInterface.java
+++ implementations/wpilib/CameraInterface.java
@@ -4,6 +4,10 @@ import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.VideoSource;
 import edu.wpi.first.wpilibj.CameraServer;
 import interfaces.VidSourceInterface;
+import lib.LogFactory;
+import lib.Logger;
+import lib.Logger.Level;
+
 import org.opencv.core.Mat;
 
 public class CameraInterface implements interfaces.CameraInterface {
@@ -11,12 +15,11 @@ public class CameraInterface implements interfaces.CameraInterface {
 	private CvSource vidSource;
 
 	@Override
-	public void startAutomaticCapture(VidSourceInterface camera) {
-		for (VideoSource source : VideoSource.enumerateSources()) {
-			if (source.getName() == camera.getName()) {
-				CameraServer.getInstance().startAutomaticCapture(source);
-				return;
-			}
+	public void startAutomaticCapture() {
+		try{
+			CameraServer.getInstance().startAutomaticCapture(VideoSource.enumerateSources()[0]);
+		} catch(Exception e){
+			LogFactory.getInstance("Camera").log(Level.ERROR, e.toString());
 		}
 	}
 
